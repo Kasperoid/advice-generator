0b77e10c5184a00ddd5ae3108cda986e3f46d95b
Kasperoid <ivanu9053@gmail.com>
2024-08-29 14:11:07 +0300

    Создание фона

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Layout, Flex } from 'antd';
+import AdviceCardContainer from './components/AdviceCardContainer';
+import { AdviceType } from './types/advice';
+import { FETCH_URL } from './constants/constants';
+import { isAdvice } from './helpers/typeGuard';
 
 function App() {
-  return (
-    <div className="App">
+  const { Content } = Layout;
+  const [advice, setAdvice] = useState<AdviceType | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    </div>
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch(FETCH_URL);
+    const data = (await response.json()) as AdviceType;
+    if (isAdvice(data)) {
+      setAdvice(data);
+    } else {
+      setAdvice(null);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      <Content>
+        <Flex justify="center" align="center">
+          <AdviceCardContainer
+            id={advice?.slip.id}
+            advice={advice?.slip.advice}
+            loading={loading}
+            fetchData={fetchData}
+          />
+        </Flex>
+      </Content>
+    </Layout>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'normalize.css';
+import './styles/app/app.css';
+import './styles/card/card.css';
+import './styles/main/main.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
