0b6d81c13bb6281e80d0ea5eb3d1879c28d845fd
Kasperoid <ivanu9053@gmail.com>
2024-09-22 17:23:22 +0300

    Добавление объекта темы

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export const baseTheme = {
+  color: {
+    primary: 'hsl(193, 38%, 86%)',
+    secondary: 'hsl(150, 100%, 66%)',
+    tertiary: 'hsl(218, 20%, 24%)',
+    quaternary: 'rgb(31, 38, 50)',
+  },
+};
