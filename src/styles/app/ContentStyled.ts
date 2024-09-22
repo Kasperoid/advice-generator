6f2b158d8e6e05a7cd3316502be944f2f8f796ab
Kasperoid <ivanu9053@gmail.com>
2024-09-22 17:23:48 +0300

    Стилизация компоненты content

---
+++ src/styles/app/ContentStyled.ts
@@ -0,0 +1,10 @@
+import { Layout } from 'antd';
+import styled from 'styled-components';
+
+const { Content } = Layout;
+
+export const ContentStyled = styled(Content)`
+  && {
+    padding: 0 15px;
+  }
+`;
