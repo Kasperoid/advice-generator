da4ba0d2985f1f258001095951dec8c9687b667c
Kasperoid <ivanu9053@gmail.com>
2024-09-22 18:26:02 +0300

    Добавление базовых стилей

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,50 @@
-export const baseTheme = {
+type colorType = {
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  quaternary: string;
+};
+
+type fontSizeType = {
+  max: number;
+  min: number;
+};
+
+type textType = {
+  cardBody: fontSizeType;
+};
+
+type titleType = {
+  cardTitle: fontSizeType;
+};
+
+type baseThemeType = {
+  color: colorType;
+  font: {
+    text: textType;
+    title: titleType;
+  };
+};
+
+export const baseTheme: baseThemeType = {
   color: {
     primary: 'hsl(193, 38%, 86%)',
     secondary: 'hsl(150, 100%, 66%)',
     tertiary: 'hsl(218, 20%, 24%)',
     quaternary: 'rgb(31, 38, 50)',
+  },
+  font: {
+    text: {
+      cardBody: {
+        max: 28,
+        min: 15,
+      },
+    },
+    title: {
+      cardTitle: {
+        max: 12,
+        min: 8,
+      },
+    },
   },
 };
